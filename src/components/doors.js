d9f13698853390631f4aea72c91b564f0f0b035d
nirkraw <nirkaw@gmail.com>
2020-08-13 17:58:02 -0400

    add background image

--- src/components/doors.js
+++ src/components/doors.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import DoorsCSS from "../styles/doors.css";
-
+import Image from "../assets/matrixhallway.jpg"
 export default class Doors extends Component {
 
   render() {
     return (
-      <div>
+      <div className="doors-main-div">
+        <img className="hallway" src={Image}></img>
         <div className="elevator-frame">
           <div className="door-left"></div>
           <div className="door-right"></div>
