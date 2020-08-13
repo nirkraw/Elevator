3c881fc0a20895d08dd99ccb76250f2e86bf3d5f
nirkraw <nirkaw@gmail.com>
2020-08-13 10:32:04 -0400

    finish layout for panel, marker, and doors

--- src/components/floorMarker.js
+++ src/components/floorMarker.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import FloorMarkerCSS from "../styles/floorMarker.css"
 
 export default class FloorMarker extends Component {
     render() {
+        const currentFloor = this.props.currentFloor === 0 ? "L" : this.props.currentFloor;
         return (
-            <div>
-                <h1>Test</h1>
+            <div className="floor-marker-outer">
+                <div className="floor-marker-box">
+                    <h1>{currentFloor}</h1>
+                </div>
             </div>
         )
     }
--- src/components/panel.js
+++ src/components/panel.js
@@ -1,33 +1,46 @@
-import React, { Component } from 'react'
-import PanelCSS from "../styles/panel.css"
-import Doors from "./doors.js"
-import FloorMarker from "./floorMarker"
+import React, { Component } from "react";
+import PanelCSS from "../styles/panel.css";
+import Doors from "./doors.js";
+import FloorMarker from "./floorMarker";
 
 export default class Panel extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             currentFloor: 0,
-             floorsPressed: []
-        }
-    }
+  constructor(props) {
+    super(props);
 
-    createPanel() {
-        return Array(8).fill().map((_, i) => <div className="floor-button"><h1>{i}</h1></div>);
-    }
-    
-    render() {
-        const panel = this.createPanel();
-        return (
-            <div className="main-view">
-                <FloorMarker />
-                <Doors currentFloor= {this.state.currentFloor}/>
-                <div className="main-panel-box">
-                    {panel}
-                </div>
-            </div>
+    this.state = {
+      currentFloor: 0,
+      floorsPressed: [],
+    };
+  }
 
+  createPanel() {
+    return Array(8)
+      .fill()
+      .map((_, i) =>
+        i !== 0 ? (
+          <div className="floor-button">
+            <h1>{i}</h1>
+          </div>
+        ) : (
+          <div className="floor-button">
+            <h1>L</h1>
+          </div>
         )
-    }
+      );
+  }
+
+  render() {
+    const panel = this.createPanel();
+    return (
+      <div className="main-view">
+        <div className="floor-marker-main">
+          <FloorMarker currentFloor={this.state.currentFloor} />
+        </div>
+        <div className="doors-and-panel-main">
+          <Doors currentFloor={this.state.currentFloor} />
+          <div className="main-panel-box">{panel}</div>
+        </div>
+      </div>
+    );
+  }
 }
